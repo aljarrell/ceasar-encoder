0fcc235b80f278cc7bcca36f80570a9583fadb39
aljarrell <alan.k.jarrell@gmail.com>
2018-11-05 16:08:48 -0500

    failing test

---
+++ tdd_ceasar.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require_relative "ceasar.rb"
+
+class My_test < Minitest::Test
+
+  def test_one
+    assert_equal(1,1)
+  end
+
+  def test_for_a
+    assert_equal("", encoder("t"))
+  end
+
+end
