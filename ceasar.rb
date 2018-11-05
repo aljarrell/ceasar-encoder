18d513fa9ee1338aaa742f26e54ca5f557010a09
aljarrell <alan.k.jarrell@gmail.com>
2018-11-05 16:51:00 -0500

    decoder function works

--- ceasar.rb
+++ ceasar.rb
@@ -1,3 +1,8 @@
 def encoder(string)
   return string.to_s.tr('a-u, v-z, A-U, V-Z, 0-4, 5-9', 'f-z, a-e, F-Z, A-E, 5-9, 0-4')
 end
+
+
+def decoder(string)
+  return string.to_s.tr('f-z, a-e, F-Z, A-E, 5-9, 0-4', 'a-u, v-z, A-U, V-Z, 0-4, 5-9')
+end
