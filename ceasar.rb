3a6e6a8b324cc76857bf9f271d1e38fac4fe7b58
aljarrell <alan.k.jarrell@gmail.com>
2018-11-06 22:35:56 -0500

    passing tests for daily encoder

--- ceasar.rb
+++ ceasar.rb
@@ -1,11 +1,26 @@
-def encoder(string)
-  return string.to_s.tr('a-u, v-z, A-U, V-Z, 0-4, 5-9', 'f-z, a-e, F-Z, A-E, 5-9, 0-4')
-end
+# def encoder(string)
+#   return string.to_s.tr('a-u, v-z, A-U, V-Z, 0-4, 5-9', 'f-z, a-e, F-Z, A-E, 5-9, 0-4')
+# end
+#
+# def decoder(string)
+#   return string.to_s.tr('f-z, a-e, F-Z, A-E, 5-9, 0-4', 'a-u, v-z, A-U, V-Z, 0-4, 5-9')
+# end
 
-def decoder(string)
-  return string.to_s.tr('f-z, a-e, F-Z, A-E, 5-9, 0-4', 'a-u, v-z, A-U, V-Z, 0-4, 5-9')
-end
+# def daily_encoder(message, day)
+#   #return message.to_s.tr('a-zA-Z',[*"a".."z", *"A".."Z"].rotate(day).join).to_s
+# end
 
 def daily_encoder(message, day)
-  return message.to_s.tr("a-z, A-Z",[*"a".."z", *"A".."Z"].rotate(day).join).to_s
+  #Transforming each character into it's ASCII number
+  transform = message.chars.map{ |x| x.ord}
+  #p transform
+  arr = Array.new
+  #iterating over the transform array and adding variable v to the arr array
+  transform.each do |v|
+    arr << v + day
+  end
+#p arr
+  #changing the ASCII numbers back into characters
+  character = arr.map { |c| c.chr}.join
+#  p character
 end
--- tdd_ceasar.rb
+++ tdd_ceasar.rb
@@ -7,45 +7,45 @@ class My_test < Minitest::Test
     assert_equal(1,1)
   end
 
-  def test_for_y
-    assert_equal("y", encoder("t"))
-  end
-
-  def test_for_stu
-    assert_equal("xyz", encoder("stu"))
-  end
-
-  def test_for_abc
-    assert_equal("fgh", encoder("abc"))
-  end
-
-  def test_for_hello_world
-    assert_equal("mjqqt btwqi", encoder("hello world"))
-  end
-
-  def test_for_uppercase_y
-    assert_equal("D", encoder("Y"))
-  end
-
-  def test_for_symbols
-    assert_equal("*", encoder("*"))
-  end
-
-  def test_for_numbers
-    assert_equal("6", encoder(1))
-  end
-
-  def test_for_decoder_fgh
-    assert_equal("abc", decoder("fgh"))
-  end
-
-  def test_decoder_for_numbers
-    assert_equal("1", decoder(6))
-  end
-
-  def test_decoder_for_symbols
-    assert_equal("*", decoder("*"))
-  end
+  # def test_for_y
+  #   assert_equal("y", encoder("t"))
+  # end
+  #
+  # def test_for_stu
+  #   assert_equal("xyz", encoder("stu"))
+  # end
+  #
+  # def test_for_abc
+  #   assert_equal("fgh", encoder("abc"))
+  # end
+  #
+  # def test_for_hello_world
+  #   assert_equal("mjqqt btwqi", encoder("hello world"))
+  # end
+  #
+  # def test_for_uppercase_y
+  #   assert_equal("D", encoder("Y"))
+  # end
+  #
+  # def test_for_symbols
+  #   assert_equal("*", encoder("*"))
+  # end
+  #
+  # def test_for_numbers
+  #   assert_equal("6", encoder(1))
+  # end
+  #
+  # def test_for_decoder_fgh
+  #   assert_equal("abc", decoder("fgh"))
+  # end
+  #
+  # def test_decoder_for_numbers
+  #   assert_equal("1", decoder(6))
+  # end
+  #
+  # def test_decoder_for_symbols
+  #   assert_equal("*", decoder("*"))
+  # end
 
   def test_for_10th_day_of_month
     assert_equal("k", daily_encoder("a", 10))
